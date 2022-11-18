098a2aa3c1a6a3791abcf427ae27cc4523d6c265
Jonatandree <javasqueza@unah.hn>
2022-11-17 23:43:01 -0600

    se agrega el cambio de estado de las ordenes y factura

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,11 +1,12 @@
 const option_menus =0;
 const option_menu = 1;
-const option_entrar = 2;
+const option_entrar =2;
 const option_registrar = 3;
 const option_ingreso = 4;
 const option_registro = 5;
 const option_ordenes = 6;
 const option_ordene = 7;
+const option_factura = 8;
 
 
 const body = document.body;
@@ -30,11 +31,13 @@ const selectOption = (option) =>{
             document.getElementById('section5').style.display="none";
             document.getElementById('section6').style.display="none";
             document.getElementById('section7').style.display="none";
+            document.getElementById('section8').style.display="none";
           
             
         break;
         case option_entrar:
             console.log('Visualizar Entrada');
+            
             body.style.backgroundColor = color2;
             document.getElementById('section1').style.display="none";
             document.getElementById('section2').style.display="block";
@@ -43,7 +46,7 @@ const selectOption = (option) =>{
             document.getElementById('section5').style.display="none";
             document.getElementById('section6').style.display="none";
             document.getElementById('section7').style.display="none";
-           
+            document.getElementById('section8').style.display="none";
         break;
         case option_registrar:
             console.log('Visualizar Registro');
@@ -55,6 +58,8 @@ const selectOption = (option) =>{
             document.getElementById('section5').style.display="none";
             document.getElementById('section6').style.display="none";
             document.getElementById('section7').style.display="none";
+            document.getElementById('section8').style.display="none";
+        break;
             
         case option_ingreso:
             console.log('Ingreso');
@@ -66,6 +71,7 @@ const selectOption = (option) =>{
             document.getElementById('section5').style.display="none";
             document.getElementById('section6').style.display="none";
             document.getElementById('section7').style.display="none";
+            document.getElementById('section8').style.display="none";
            
         break;
         case option_registro:
@@ -78,6 +84,7 @@ const selectOption = (option) =>{
             document.getElementById('section5').style.display="block";
             document.getElementById('section6').style.display="none";
             document.getElementById('section7').style.display="none";
+            document.getElementById('section8').style.display="none";
           
         break;
         case option_ordenes:
@@ -90,6 +97,7 @@ const selectOption = (option) =>{
             document.getElementById('section5').style.display="none";
             document.getElementById('section6').style.display="block";
             document.getElementById('section7').style.display="none";
+            document.getElementById('section8').style.display="none";
             
         break;
         case option_ordene:
@@ -102,6 +110,21 @@ const selectOption = (option) =>{
             document.getElementById('section5').style.display="none";
             document.getElementById('section6').style.display="none";
             document.getElementById('section7').style.display="block";
+            document.getElementById('section8').style.display="none";
+            
+            
+        break;
+        case option_factura:
+            console.log('vemos la factura');
+            body.style.backgroundColor = color3;
+            document.getElementById('section1').style.display="none";
+            document.getElementById('section2').style.display="none";
+            document.getElementById('section3').style.display="none";
+            document.getElementById('section4').style.display="none";
+            document.getElementById('section5').style.display="none";
+            document.getElementById('section6').style.display="none";
+            document.getElementById('section7').style.display="none";
+            document.getElementById('section8').style.display="block";
             
             
         break;
@@ -137,10 +160,10 @@ var estados = [
 
 
 function GenerarEstados(){
-    estados.forEach(function(estado){
+    estados.forEach(function(estado,i){
         document.getElementById('estados').innerHTML +=
-        `<div style="margin-bottom: 10px;"  class="col-9"  id="fondo-estados-orden"> 
-        <i style="font-size: 100px; display: flex; justify-content: center; color: #fff; padding-top:20px;" class='${estado.signo}'></i>
+        `<div onclick="CambioEstilo(${i})"  style="margin-bottom: 10px;"  class="col-9"  id="fondo-estados-orden"> 
+        <i id="logo${i}"; style="font-size: 100px; display: flex; justify-content: center; color: #fff; padding-top:20px;" class='${estado.signo}'></i>
         <h1 style=" display: flex;justify-content:center ;color:#fff;; ">${estado.estado}</h1>
     </div>`
 
@@ -150,3 +173,57 @@ function GenerarEstados(){
 }
 
 GenerarEstados();
+
+function CambioEstilo(i){
+    console.log('Confirmar Orden');
+    document.getElementById(`logo${i}`).style.color = 'rgb(8, 187, 47)';
+
+}
+function RemoverEstilo(){
+    console.log('regresar el estado');
+    document.getElementById(`logo0`).style.color = 'rgb(255, 255, 255)';
+    document.getElementById(`logo1`).style.color = 'rgb(255, 255, 255)';
+    document.getElementById(`logo2`).style.color = 'rgb(255, 255, 255)';
+    document.getElementById(`logo3`).style.color = 'rgb(255, 255, 255)';
+
+}
+
+
+
+var EstadoDeRegistro = [
+
+    { 
+        estado: "Estado: Pendiente",
+        signo:"bx bxs-info-circle",
+    },
+    { 
+        estado: "Estado: Aprobado",
+        signo:"bx bxs-check-circle",
+    },
+    { 
+        estado: "Estado: Denegado",
+        signo:"bx bxs-x-circle",
+    },
+];
+
+
+
+function GenerarEstadosDeRegistro(){
+    EstadoDeRegistro.forEach(function(estado){
+        document.getElementById('EstadosDeRegistro').innerHTML +=
+        `<div style="margin-bottom: 10px;"  class="col-12"  id="fondo-registro"> 
+        <div style=" margin-top:20px">
+        <h1 style=" display: flex;justify-content:center ;color:#fff;; ">¡El registro se ha</h1>
+        <h1 style=" display: flex;justify-content:center ;color:#fff;; ">realizado exitosamente!</h1>
+        </div>
+
+        <i style="font-size: 200px; display: flex; justify-content: center; color: #fff; padding-top:20px;" class='${estado.signo}'></i>
+        <h1 style=" font-size:35px;margin-top:20px; display: flex;justify-content:center ;color:#fff; ">${estado.estado}</h1>
+    </div>`
+
+
+    });
+
+}
+
+GenerarEstadosDeRegistro();
